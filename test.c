a3b17e8741849a483204c0c3d06fea2fc3e8515c
99003575 <78539650+99003575@users.noreply.github.com>
2021-03-02 11:36:21 +0530

    Update test.c

--- test.c
+++ test.c
@@ -8,23 +8,23 @@ int main()
   char s[]="linux activity";
   char str1[20]="linux program";
   char str2[20];
-  char str3[100]="This is ",str4[]="Harshitha";
+  char str3[100]="Hello",str4[]="Harshitha";
   char str5[]="Harshitha",str6[]="HarshITha";
-  int prime=3,palindrome=14241,fact1=6,num=3,k=1;
+  int prime=3,palindrome=14241,fact=6,num=5,k=1;
   
   printf("length of the string is %lu\n ",mystrlen(s));
-  printf("coping of a string is %lu\n ",mystrcpy(str2,str1));
-  printf("concatenation of the string is %lu\n ",mystrcat(str3,str4));
-  printf("comparison of the string is %lu\n ",mystrcmp(str5,str6));
+  printf("coping of string is %lu\n ",mystrcpy(str2,str1));
+  printf("concatenation of string is is %lu\n ",mystrcat(str3,str4));
+  printf("comparison of string is %lu\n ",mystrcmp(str5,str6));
   
 
   if(isPrime(prime))
-      printf("the number is Prime\n");
+      printf("the number is prime\n");
 
   if(isPalindrome(palindrome))
-      printf("the number is Palindrome\n");
+      printf("the number is palindrome\n");
 
-  printf("factorial of %d is %d \n",fact,factorial(fact1));
+  printf("factorial of %d is %d \n",fact,Factorial(fact));
   printf("setting %d th of %d is %d \n",k,num,set(num,k));
   printf("setting %d th of %d is %d \n",k,num,reset(num,k));
   printf("setting %d th of %d is %d \n",k,num,flip(num,k));
