c5aa0bed80291d2942db9c03d4a4e3ff9bc127a2
99003575 <78539650+99003575@users.noreply.github.com>
2021-03-02 11:32:19 +0530

    Update bitmask.h

--- include/bitmask.h
+++ include/bitmask.h
@@ -1,7 +1,7 @@
 #ifndef __BITMASK_H
 #define __BITMASK_H
-int set(int a, int b);
-int reset(int a, int b);
-int flip(int a, int b);
-int query(int a,int b);
+int set(int a, int u);
+int reset(int a, int u);
+int flip(int a, int u);
+int query(int a,int u);
 #endif
