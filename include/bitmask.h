0d2dae7a5500c8453dfe2127d9b6c2d6a55ef597
99003575 <78539650+99003575@users.noreply.github.com>
2021-03-02 08:57:00 +0530

    Add files via upload

---
+++ include/bitmask.h
@@ -0,0 +1,7 @@
+#ifndef __BITMASK_H
+#define __BITMASK_H
+int set(int a, int b);
+int reset(int a, int b);
+int flip(int a, int b);
+int query(int a,int b);
+#endif
---
+++ include/myutils.h
@@ -0,0 +1,6 @@
+#ifndef __MYUTILS_H
+#define __MYUTILS_H
+int factorial(int);
+int isPrime(int m);
+int isPalindrome(int n);
+#endif
