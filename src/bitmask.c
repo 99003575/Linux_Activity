eeca2ef35d3da7bbddc9cc5cd0296a592e983328
99003575 <78539650+99003575@users.noreply.github.com>
2021-03-02 11:34:07 +0530

    Update bitmask.c

--- src/bitmask.c
+++ src/bitmask.c
@@ -1,24 +1,24 @@
 #include "bitmask.h"
 
 //Function to set bit
-int set(int a, int b) 
+int set(int a, int u) 
 { 
-    return (a | (1 << (b - 1))); 
+    return (a | (1 << (u - 1))); 
 } 
   
-// Function to reset the bth bit of a 
-int reset(int a, int b) 
+// Function to reset the uth bit of a 
+int reset(int a, int u) 
 { 
-    return (a & (~(1 << (b - 1)))); 
+    return (a & (~(1 << (u - 1)))); 
 } 
   
-// Function to toggle the bth bit of a
-int flip(int a, int b)
+// Function to toggle the uth bit of a
+int flip(int a, int u)
 { 
-    return (a ^ (1 << (b - 1))); 
+    return (a ^ (1 << (u - 1))); 
 } 
 
-int query(int a,int b)
+int query(int a,int u)
 {
 
 }
