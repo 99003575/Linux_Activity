9943f3ceaf58435cae89247efff419401fe29143
99003575 <78539650+99003575@users.noreply.github.com>
2021-03-04 08:18:42 +0530

    Add files via upload

---
+++ Linux_Activity_2/Threads/Second/threads_main.c
@@ -0,0 +1,10 @@
+#include "maxmin.h"
+
+int main()
+{
+  /* Find maximum number */
+  printf("maximum number : %d\n", findmax());
+  /* Find minimum NUmber */
+  printf("minimum number : %d\n", findmin());
+  return 0;
+}
